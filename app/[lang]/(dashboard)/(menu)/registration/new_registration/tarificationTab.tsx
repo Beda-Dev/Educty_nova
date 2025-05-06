2f9341e981ab08886b356ce07a2e34651cfa548b
Beda-Dev <dinguibedajunior@gmail.com>
2025-05-06 21:47:31 +0000

    setup

--- app/[lang]/(dashboard)/(menu)/registration/new_registration/tarificationTab.tsx
+++ app/[lang]/(dashboard)/(menu)/registration/new_registration/tarificationTab.tsx
@@ -5,6 +5,7 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@
 import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
 interface Props {
